cd22783e285f2dadbf8953d298d400da2a907d93
Harshit Dubey <hdubey@servicesats.com>
2024-02-21 11:35:37 +0530

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL:'https://pzmmapi.a10s.in/api/',
+  // baseURL:'https://pzmmapi.a10s.in/api/',
+  baseURL:'http://localhost:5001/api/',
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
 });
 
